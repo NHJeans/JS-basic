9815bd609081b7760e1e7fee3bdf960d36a61d79
NHJeans <guswo1013@naver.com>
2024-04-26 19:40:41 +0900

    :sparkles:기능구현완료

---
+++ src/index.js
@@ -0,0 +1,119 @@
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzA5MTZmN2I0NzQyMGFhMjYzNWJlY2E2NjFjMjVkZiIsInN1YiI6IjY2MjYwOTAyNjNkOTM3MDE4Nzc0MGZiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lj5NbVCVHmBGRLI-iHDmhau9pA4XE04pa-SrqG3_Zlc';
+const BASE_URL = 'https://api.themoviedb.org/3'
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/'
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  },
+};
+
+
+let moviesData = [];
+
+const getMovies = async () => {
+  try {
+    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1', options).json();
+    const data = await response.json();
+    // console.log(data.results)
+    moviesData = data.results;
+    renderMovies(moviesData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const createMovieCard = moviesData => {
+  const { id, poster_path, title, overview, vote_average } = moviesData;
+
+  //? 영화 카드의 컨테이너를 생성
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('movie-card');
+  movieCard.id = id;
+
+  //? 영화 포스터 이미지
+  const img = document.createElement('img');
+  img.src = `${IMAGE_URL}${poster_path}`;
+  img.alt = `${title} poster`;
+  img.classList.add('movie-poster');
+
+  const contentContainer = document.createElement('div');
+  contentContainer.classList.add('content-container');
+
+
+  //? 영화 제목
+  const movieTitle = document.createElement('h3');
+  movieTitle.textContent = title;
+  movieTitle.classList.add('movie-title');
+
+  //? 영화 설명
+  const movieOverview = document.createElement('p');
+  movieOverview.textContent = overview;
+  movieOverview.classList.add('movie-overview');
+
+  //? 평점
+  const movieRating = document.createElement('span');
+  movieRating.textContent = `Rating: ${vote_average}`;
+  movieRating.classList.add('movie-rating');
+
+
+  movieCard.appendChild(img);
+  movieCard.appendChild(contentContainer);
+
+  contentContainer.appendChild(movieTitle);
+  contentContainer.appendChild(movieOverview);
+  contentContainer.appendChild(movieRating);
+
+  movieCard.addEventListener('click', showMovieId);
+
+  return movieCard;
+}
+
+const renderMovies = moviesData => {
+  const moviesContainer = document.querySelector('.card-list');
+  moviesContainer.innerHTML = '';
+
+  moviesData.forEach(movie => {
+    const movieCard = createMovieCard(movie);
+    moviesContainer.appendChild(movieCard);
+  });
+}
+
+const showMovieId = (e) => {
+  window.alert(`영화 ID: ${e.currentTarget.id}`);
+};
+
+
+const searchMovies = (movies) => {
+  const searchText = document.getElementById('search-input').value.toLowerCase();
+  // console.log(document.getElementById('search-input').value);
+  if (searchText === '') {
+    alert("검색어를 입력해주세요.");
+    return
+  }
+  const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchText));
+  renderMovies(filteredMovies);
+}
+
+document.getElementById('search-btn').addEventListener('click', function (e) {
+  e.preventDefault();
+  searchMovies(moviesData);
+});
+document.querySelector('header h1').addEventListener(
+  'click', () => {
+    window.location.reload();
+  });
+document.querySelector('.up-btn').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+getMovies();
+
+
+
