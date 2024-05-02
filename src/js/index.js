176d835acad620f56539d1cc20b04010716b2d2d
heesu21 <gmltncpfl1@naver.com>
2024-05-02 20:24:27 +0900

    Feat: main swiper add

--- src/js/index.js
+++ src/js/index.js
@@ -116,5 +116,23 @@ document.querySelector('.up-btn').addEventListener('click', () => {
 
 getMovies();
 
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    
+
+
+
+
 
 
