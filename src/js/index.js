489e0ec1b006dcc99fab2dcbd54f9c971484fe1a
백현명 <whitewise95@users.noreply.github.com>
2024-05-03 20:56:51 +0900

    Fix: 병합 해결

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,15 @@
-const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzA5MTZmN2I0NzQyMGFhMjYzNWJlY2E2NjFjMjVkZiIsInN1YiI6IjY2MjYwOTAyNjNkOTM3MDE4Nzc0MGZiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lj5NbVCVHmBGRLI-iHDmhau9pA4XE04pa-SrqG3_Zlc';
-const BASE_URL = 'https://api.themoviedb.org/3'
-const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/'
-
+const API_KEY =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzA5MTZmN2I0NzQyMGFhMjYzNWJlY2E2NjFjMjVkZiIsInN1YiI6IjY2MjYwOTAyNjNkOTM3MDE4Nzc0MGZiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lj5NbVCVHmBGRLI-iHDmhau9pA4XE04pa-SrqG3_Zlc';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
 
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
+    Authorization: `Bearer ${API_KEY}`,
   },
 };
-
 
 let moviesData = [];
 
@@ -24,9 +23,9 @@ const getMovies = async () => {
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-const createMovieCard = moviesData => {
+const createMovieCard = (moviesData) => {
   const { id, poster_path, title, overview, vote_average } = moviesData;
 
   //? 영화 카드의 컨테이너를 생성
@@ -43,7 +42,6 @@ const createMovieCard = moviesData => {
   const contentContainer = document.createElement('div');
   contentContainer.classList.add('content-container');
 
-
   //? 영화 제목
   const movieTitle = document.createElement('h3');
   movieTitle.textContent = title;
@@ -59,7 +57,6 @@ const createMovieCard = moviesData => {
   movieRating.textContent = `Rating: ${vote_average}`;
   movieRating.classList.add('movie-rating');
 
-
   movieCard.appendChild(img);
   movieCard.appendChild(contentContainer);
 
@@ -70,43 +67,41 @@ const createMovieCard = moviesData => {
   movieCard.addEventListener('click', showMovieId);
 
   return movieCard;
-}
+};
 
-const renderMovies = moviesData => {
+const renderMovies = (moviesData) => {
   const moviesContainer = document.querySelector('.card-list');
   moviesContainer.innerHTML = '';
 
-  moviesData.forEach(movie => {
+  moviesData.forEach((movie) => {
     const movieCard = createMovieCard(movie);
     moviesContainer.appendChild(movieCard);
   });
-}
+};
 
 const showMovieId = (e) => {
   window.alert(`영화 ID: ${e.currentTarget.id}`);
 };
 
-
 const searchMovies = (movies) => {
   const searchText = document.getElementById('search-input').value.toLowerCase();
   // console.log(document.getElementById('search-input').value);
   if (searchText === '') {
-    alert("검색어를 입력해주세요.");
-    return
+    alert('검색어를 입력해주세요.');
+    return;
   }
-  const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchText));
+  const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(searchText));
   renderMovies(filteredMovies);
-}
+};
 
 document.getElementById('search-form').addEventListener('submit', function (e) {
   e.preventDefault();
   searchMovies(moviesData);
 });
 // 메인으로 돌아가는 버튼
-document.querySelector('header h1').addEventListener(
-  'click', () => {
-    window.location.reload();
-  });
+document.querySelector('header h1').addEventListener('click', () => {
+  window.location.reload();
+});
 document.querySelector('.up-btn').addEventListener('click', () => {
   window.scrollTo({
     top: 0,
@@ -129,22 +124,16 @@ const swiperOption = {
   centeredSlides: true,
   spaceBetween: 15,
   pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
+    el: '.swiper-pagination',
+    type: 'fraction',
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-}
+};
 
-const mainSwiper = new Swiper(".mySwiper", swiperOption);
-const subSwiper = new Swiper(".subSwiper", swiperOption);
+getMovies();
 
-    
-
-
-
-
-
-
+const mainSwiper = new Swiper('.mySwiper', swiperOption);
+const subSwiper = new Swiper('.subSwiper', swiperOption);
